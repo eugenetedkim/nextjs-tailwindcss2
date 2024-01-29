9fd28e1934e901c364b87435bd91db6b6365fec3
eugenetedkim <eugenetedkim@gmail.com>
2024-01-29 00:39:50 -0800

    Add fix to mobile display of blog posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -62,6 +62,10 @@ const components = {
 export default function Post({ mdxSource }) {
   return (
     <>
+      <Head>
+        <title>{mdxSource.frontmatter.title}</title>
+      </Head>
+
       <Hero
         image={heroBackgroundImageBlog}
         heading={mdxSource.frontmatter.title}
@@ -70,17 +74,15 @@ export default function Post({ mdxSource }) {
         link={`#${mdxSource.frontmatter.title.replace(/\s/g, '')}`}
       />
 
-        <div id={mdxSource.frontmatter.title.replace(/\s/g, '')} className='flex justify-center p-12 scroll-my-40 mt-24 mb-12'>
-          <Head>
-            <title>{mdxSource.frontmatter.title}</title>
-          </Head>
-
-          <div className='prose flex flex-col justify-center max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto mt-6'>
-            <MDXProvider components={components}>
-              <MDXRemote {...mdxSource } components={{...components}} />
-            </MDXProvider>
-          </div>
-          <ScrollToTop />
+      <div id={mdxSource.frontmatter.title.replace(/\s/g, '')}
+        className='flex justify-center p-12 scroll-my-40 mt-24 mb-12'
+      >
+        <div className='prose flex flex-col justify-center max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl mt-6'>
+          <MDXProvider components={components}>
+            <MDXRemote {...mdxSource } components={{...components}} />
+          </MDXProvider>
+        </div>
+        <ScrollToTop />
       </div>
 
     </>
