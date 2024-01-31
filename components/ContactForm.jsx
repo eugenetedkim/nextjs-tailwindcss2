eafcb21a4d88ffc4c87c1775ca77219f622c69ef
eugenetedkim <eugenetedkim@gmail.com>
2024-01-31 14:18:31 -0800

    Add change to fetch url

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -18,7 +18,7 @@ export default function ContactForm() {
       firstName, lastName, email, message
     }
     
-    const res = await fetch("http://localhost:3000/api/send", {
+    const res = await fetch("https://eugenetedkim.com/api/send", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(data)
