dd05137de63118835113a3e21c0154ebe9910ad6
eugenetedkim <eugenetedkim@gmail.com>
2024-01-31 16:48:05 -0800

    Add change to fetch url

--- pages/api/send.js
+++ pages/api/send.js
@@ -7,22 +7,25 @@ export default async (req, res) => {
   const requestBody = req.body;
   console.log(requestBody);
   console.log(req.method);
-  const firstName = requestBody.firstName;
-  const lastName = requestBody.lastName;
-  const email = requestBody.email;
-  const message = requestBody.message;
-  console.log('sending email');
-  const { data, error } = await resend.emails.send({
-    from: 'eugene@eugenetedkim.com',
-    to: ['eugenetedkim@gmail.com'],
-    subject: 'Message from your portfolio site',
-    // html: '<div>Hi</div>'
-    react: EmailTemplate({firstName, lastName, email, message}),
-  });
+  if (req.method == 'POST') {
 
-  if (error) {
-    return res.status(400).json(error);
+    const firstName = requestBody.firstName;
+    const lastName = requestBody.lastName;
+    const email = requestBody.email;
+    const message = requestBody.message;
+    console.log('sending email');
+    const { data, error } = await resend.emails.send({
+      from: 'eugene@eugenetedkim.com',
+      to: ['eugenetedkim@gmail.com'],
+      subject: 'Message from your portfolio site',
+      // html: '<div>Hi</div>'
+      react: EmailTemplate({firstName, lastName, email, message}),
+    });
+  
+    if (error) {
+      return res.status(400).json(error);
+    }
+  
+    res.status(200).json(data);
   }
-
-  res.status(200).json(data);
 };
