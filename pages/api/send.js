8dd86e2563578899a7b54e85b6f384908f51722e
eugenetedkim <eugenetedkim@gmail.com>
2024-01-31 16:41:16 -0800

    Add change to fetch url

--- pages/api/send.js
+++ pages/api/send.js
@@ -4,11 +4,13 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req, res) => {
-  console.log(req.body);
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
+  const requestBody = req.body;
+  console.log(requestBody);
+  console.log(req.method);
+  const firstName = requestBody.firstName;
+  const lastName = requestBody.lastName;
+  const email = requestBody.email;
+  const message = requestBody.message;
   console.log('sending email');
   const { data, error } = await resend.emails.send({
     from: 'eugene@eugenetedkim.com',
