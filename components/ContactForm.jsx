d7dc08225e47c88976c395913b57952c49416d7e
eugenetedkim <eugenetedkim@gmail.com>
2024-01-31 17:17:17 -0800

    Add change to fetch url

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -20,9 +20,9 @@ export default function ContactForm() {
     
     const res = await fetch("/api", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
       body: JSON.stringify(data)
     });
 
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className='font-html scroll-smooth'>
-      <Head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
